8487135474ddb125ccbf6efb5e34318a275717eb
Scott Walters <scottw567@gmail.com>
2025-09-13 15:46:04 -0400

    userService tests implemented

--- backend/__test__/userService.test.js
+++ backend/__test__/userService.test.js
@@ -4,10 +4,16 @@ const userRepository = require("../repositories/userRepository");
 const userService = require("../services/userService");
 // maybe put in describe block
 
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
 describe("User Service Tests", () => {
   test("getUserByUsername should return the user", async () => {
     userRepository.getUserByUsername.mockResolvedValue({
+      createdAt: 1757705534436,
       username: "Alice",
+      role: "employee",
     });
 
     const result = await userService.getUserByUsername({
@@ -15,11 +21,27 @@ describe("User Service Tests", () => {
     });
 
     expect(result).toEqual({
+      createdAt: 1757705534436,
       username: "Alice",
+      role: "employee",
     });
 
     expect(userRepository.getUserByUsername).toHaveBeenCalledWith("Alice");
+    expect(userRepository.getUserByUsername).toHaveBeenCalledTimes(1);
   });
-  
-//   next: test user not found
+
+  test("getUserByUsername should return null if user doesn't exist", async () => {
+    userRepository.getUserByUsername.mockResolvedValue(null);
+
+    const result = await userService.getUserByUsername({
+      username: "Nonexistentuser",
+    });
+
+    expect(result).toEqual(null);
+
+    expect(userRepository.getUserByUsername).toHaveBeenCalledWith(
+      "Nonexistentuser"
+    );
+    expect(userRepository.getUserByUsername).toHaveBeenCalledTimes(1);
+  });
 });
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,13 +1,21 @@
 const userRepository = require("../repositories/userRepository");
 
-function getUserByUsername(params) {
+async function getUserByUsername(params) {
   // change this to separate username and validate maybe
-  return userRepository.getUserByUsername(params.username);
+  const user = await userRepository.getUserByUsername(params.username);
+  if (!user) return null;
+  return sanitizeUser(user);
 }
 
 function deleteUserByUsername(params) {
   // change this to separate username and validate maybe
   return userRepository.deleteUserByUsername(params.username);
+}
+
+// removing password before returning to controller
+function sanitizeUser(user) {
+  const { password, ...safeUser } = user;
+  return safeUser;
 }
 
 module.exports = {
