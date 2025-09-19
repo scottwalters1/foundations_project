89a9db006a74450aea4718a2a86ec6cf1bd3d3db
Scott Walters <scottw567@gmail.com>
2025-09-19 12:12:08 -0400

    final

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,15 +1,9 @@
 function loginFormSubmit(event) {
-        event.preventDefault(); // prevent page reload
-
+        event.preventDefault(); 
         let form = document.forms["loginForm"];
         let username = form["username"].value;
         let password = form["password"].value;
 
         console.log("Username:", username);
         console.log("Password:", password);
-        
-        // do login api call here, get token
-        // store token in localStorage or some bullshit like that
-            // or cookie
-        // now redirect with window.location
       }
